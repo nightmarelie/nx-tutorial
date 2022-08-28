9f0d7cd18a98aaa52eacee852b2bc14f7676bb79
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-28 15:50:18 -0600

    feat: update api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,12 @@
  */
 
 import * as express from 'express';
+import { ApiResponse, API_URL } from '@nx-tutorial/api-interface';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.get(API_URL, (req, res) => {
+  res.send({ message: 'Welcome to api!' } as ApiResponse);
 });
 
 const port = process.env.port || 3333;
